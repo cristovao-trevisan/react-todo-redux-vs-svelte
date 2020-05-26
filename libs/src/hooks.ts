1c32356a27f3e78ce3e1765e6aa65ac8ca6faf5b
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2020-05-26 00:32:12 -0300

    Export hooks

--- libs/src/hooks.ts
+++ libs/src/hooks.ts
@@ -5,5 +5,5 @@ import { initialState, State } from './states'
 export function useAsync <T, I> (store: AsyncStore<T>) {
   const [state, setState] = useState(initialState() as State<T>)
   useEffect(() => store.subscribe(v => setState(v)))
-  return [state, store.request, store.promise]
+  return [state, store.request]
 }
--- libs/src/index.ts
+++ libs/src/index.ts
@@ -1,3 +1,4 @@
 
 export * from './core'
 export * from './requesters'
+export * from './hooks'
